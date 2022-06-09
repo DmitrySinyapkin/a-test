a5982b7282fb123acf1544e82fc6bbc4184eec38
DmitrySinyapkin <dsinyapkin@mail.ru>
2022-06-09 14:24:56 +0300

    contacts view added

--- med-vue/src/router/index.js
+++ med-vue/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProfileView from '../views/ProfileView.vue'
+import ContactsView from '../views/ContactsView.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/profile',
     name: 'profile',
     component: ProfileView
+  },
+  {
+    path: '/contacts',
+    name: 'contacts',
+    component: ContactsView
   }
 ]
 
---
+++ med-vue/src/views/ContactsView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="contacts">
+        <h1 class="contacts__title">Контакты</h1>
+    </div>
+</template>
+
+<style lang="scss">
+.contacts {
+    padding: 220px 200px;
+
+    @media (max-width: 768px) {
+        padding: 55px 10px;
+    }
+
+    &__title {
+        font-family: 'Montserrat';
+        font-weight: 700;
+        font-size: 58px;
+        line-height: 80px;
+
+        @media (max-width: 768px) {
+            font-size: 28px;
+            line-height: 39px;
+        }
+    }
+}
+</style>
